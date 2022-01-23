e2b234bc61b862f075e8f2997e7e15999182c845
Lenka <hladkalenka@gmail.com>
2022-01-23 18:37:50 +0100

    Refactor code

--- src/main/java/cz/mendelu/ja/di/app/IOC.java
+++ src/main/java/cz/mendelu/ja/di/app/IOC.java
@@ -14,38 +14,28 @@ public class IOC {
         try {
             var instance = appClass.getDeclaredConstructor().newInstance();
             var fields = appClass.getDeclaredFields();
-//            setInstaces.add(instance);
-//            for (var field: fields) {
-//                if (field.getAnnotation(MyAtributeAnnotation.class) != null) {
-//                    field.setAccessible(true);
-//                    field.set(instance, field.getType().getDeclaredConstructor().newInstance());
-//                }
-//            }
-//
-            rekurze(fields, instance);
 
-//                System.out.println(field.getName());
-//                System.out.println(field.getAnnotation(MyClassAnnotation.class));
-//            }
+            createAppBeansRecursively(fields, instance);
+
             return instance;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void rekurze(Field[] fields, Object instance) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void createAppBeansRecursively(Field[] fields, Object instance) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
-        for (var field: fields) {
+        for (var field : fields) {
             Object actualInst = null;
             if (field.getAnnotation(MyAtributeAnnotation.class) != null) {
-                if (isBeanAlreadyCreated(field, instance)){
+                if (isBeanAlreadyCreated(field, instance)) {
                     field.setAccessible(true);
                     Object findInst = setInstaces.stream().
                             filter(inst -> inst.getClass().equals(field.getType())).findFirst().get();
                     field.set(instance, findInst);
                     actualInst = findInst;
 
-                }else {
+                } else {
                     field.setAccessible(true);
                     Object newInst = field.getType().getDeclaredConstructor().newInstance();
                     setInstaces.add(newInst);
@@ -58,7 +48,7 @@ public class IOC {
                         .findAny().isPresent();
 
                 if (containsAnotatedField && !isBeanAlreadyCreated(field, actualInst)) {
-                    rekurze(field.getType().getDeclaredFields(), actualInst);
+                    createAppBeansRecursively(field.getType().getDeclaredFields(), actualInst);
                 }
             }
         }
